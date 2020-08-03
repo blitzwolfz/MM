ef381bac3ffbcb57542d5b4978764c18acd52c3c
Sam <qureshisamin25@gmail.com>
2020-08-03 11:17:52 -0400

    Staging randomtemp files

---
+++ dist/misc/randomtemp.js
@@ -0,0 +1,98 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.RandomTemplateFunc = exports.RandomTemplate = exports.getRandomTemplateList = exports.disapprovefilter = exports.redofilter = exports.approvefilter = void 0;
+const Discord = __importStar(require("discord.js"));
+const utils_1 = require("./utils");
+const db_1 = require("./db");
+exports.approvefilter = (reaction, user) => reaction.emoji.name === utils_1.emojis[7] && !user.bot;
+exports.redofilter = (reaction, user) => reaction.emoji.name === '🌀' && !user.bot;
+exports.disapprovefilter = (reaction, user) => reaction.emoji.name === utils_1.emojis[8] && !user.bot;
+async function getRandomTemplateList(client) {
+    var templatelist = [];
+    let lastmsg = [];
+    console.time("start");
+    await client.channels.cache.get("724827952390340648").messages.fetch({ limit: 100 }).then(async (msg) => {
+        console.log(msg.map(async (m) => {
+            await client.channels.cache.get("724827952390340648").messages.fetch(m.id).then(async (m2) => {
+                if (m2.attachments.size >= 1) {
+                    for (let x = 0; x < m2.attachments.array().length; x++) {
+                        if (!m2.attachments.array()[x].url.includes("gif") || !m2.attachments.array()[0].url.includes("mp4")) {
+                            templatelist.push(m2.attachments.array()[x].url);
+                        }
+                    }
+                    lastmsg.push(m2.id);
+                }
+            });
+        }));
+    });
+    for (let i = 0; i < 4; i++) {
+        await client.channels.cache.get("724827952390340648").messages.fetch({ before: lastmsg[0], limit: 100 }).then(async (msg) => {
+            msg.map(async (m) => {
+                await client.channels.cache.get("724827952390340648").messages.fetch(m.id).then(async (m2) => {
+                    if (m2.attachments.size >= 1) {
+                        for (let x = 0; x < m2.attachments.array().length; x++) {
+                            if (!m2.attachments.array()[x].url.includes("gif") || !m2.attachments.array()[0].url.includes("mp4")) {
+                                templatelist.push(m2.attachments.array()[x].url);
+                            }
+                        }
+                        lastmsg[0] = (m2.id);
+                    }
+                });
+            });
+        });
+    }
+    let set = await new Set(templatelist);
+    templatelist = [];
+    templatelist = Array.from(set);
+    return templatelist;
+}
+exports.getRandomTemplateList = getRandomTemplateList;
+function RandomTemplate() {
+}
+exports.RandomTemplate = RandomTemplate;
+async function RandomTemplateEmbed(random, id) {
+    let embed = new Discord.MessageEmbed()
+        .setTitle("Random template")
+        .setDescription(`<#${id}>`)
+        .setImage(random)
+        .setColor("#d7be26")
+        .setTimestamp();
+    return embed;
+}
+async function RandomTemplateFunc(message, client, _id) {
+    let templatelist = await getRandomTemplateList(client);
+    let random = templatelist[Math.floor(Math.random() * (((templatelist.length - 1) - 1) - 1) + 1)];
+    let tempstruct = {
+        _id: message.channel.id,
+        found: false,
+        url: random,
+        messageid: "",
+        time: Math.floor(Date.now() / 1000)
+    };
+    await client.channels.cache.get("722616679280148504").send(await RandomTemplateEmbed(random, message.channel.id)).then(async (message) => {
+        await message.react(utils_1.emojis[7]);
+        await message.react('❌');
+        tempstruct.messageid = message.id;
+    });
+    await db_1.inserttempStruct(tempstruct);
+}
+exports.RandomTemplateFunc = RandomTemplateFunc;
---
+++ src/misc/randomtemp.ts
@@ -0,0 +1,154 @@
+import * as Discord from "discord.js"
+import { emojis } from "./utils";
+import { randomtempstruct } from "./struct";
+import { inserttempStruct } from "./db";
+
+
+export const approvefilter = (reaction: { emoji: { name: string; }; }, user: Discord.User) => reaction.emoji.name === emojis[7] && !user.bot;
+export const redofilter = (reaction: { emoji: { name: string; }; }, user: Discord.User) => reaction.emoji.name === '🌀' && !user.bot;
+export const disapprovefilter = (reaction: { emoji: { name: string; }; }, user: Discord.User) => reaction.emoji.name === emojis[8] && !user.bot;
+
+
+export async function getRandomTemplateList(client: Discord.Client): Promise<string[]>{
+
+  var templatelist:string[] = []
+  let lastmsg:string[] = []
+  console.time("start")
+  
+  await (<Discord.TextChannel>client.channels.cache.get("724827952390340648")).messages.fetch({ limit: 100 }).then(async msg => {
+    console.log(msg.map(async m => {
+      // console.log(m.url)
+      // await message.reply(m.id)
+      await (<Discord.TextChannel>client.channels.cache.get("724827952390340648")).messages.fetch(m.id).then(async (m2: Discord.Message) => {
+        if (m2.attachments.size >= 1) {
+          // console.log(m2.attachments.array()[0].url)
+          for(let x = 0; x < m2.attachments.array().length; x++){
+            if(!m2.attachments.array()[x].url.includes("gif") || !m2.attachments.array()[0].url.includes("mp4")){
+              templatelist.push(m2.attachments.array()[x].url)
+            }
+          }
+          lastmsg.push(m2.id)
+          // await message.reply(m2.attachments.array()[0].url)
+        }
+      })
+    }));
+  })
+
+  for(let i = 0; i < 4; i++){
+    await (<Discord.TextChannel>client.channels.cache.get("724827952390340648")).messages.fetch({ before:lastmsg[0], limit:100 }).then(async msg => {
+      msg.map(async m => {
+        // console.log(m.url)
+        // await message.reply(m.id)
+        await (<Discord.TextChannel>client.channels.cache.get("724827952390340648")).messages.fetch(m.id).then(async (m2: Discord.Message) => {
+          if (m2.attachments.size >= 1) {
+            // console.log(m2.attachments.array()[0].url)
+            for(let x = 0; x < m2.attachments.array().length; x++){
+              if(!m2.attachments.array()[x].url.includes("gif") || !m2.attachments.array()[0].url.includes("mp4")){
+                templatelist.push(m2.attachments.array()[x].url)
+              }
+            }
+            lastmsg[0] = (m2.id)
+            // await message.reply(m2.attachments.array()[0].url)
+          }
+        })
+      });
+    })
+  }
+
+  let set = await new Set(templatelist)
+
+  templatelist = []
+
+  templatelist = Array.from(set)
+
+  return templatelist
+  //await message.reply(`\`\`\`${templatelist}\`\`\``)
+}
+
+export function RandomTemplate(){
+
+}
+
+
+async function RandomTemplateEmbed(random: string, id:string){
+
+  let embed = new Discord.MessageEmbed()
+    .setTitle("Random template")
+    .setDescription(`<#${id}>`)
+    .setImage(random)
+    .setColor("#d7be26")
+    .setTimestamp()
+  
+  return embed
+}
+
+
+export async function RandomTemplateFunc(message: Discord.Message, client: Discord.Client, _id:string){
+
+    let templatelist = await getRandomTemplateList(client)
+    let random:string = templatelist[Math.floor(Math.random() * (((templatelist.length - 1) - 1) - 1) + 1)];
+
+    let tempstruct :randomtempstruct = {
+      _id: message.channel.id,
+      found: false,
+      url: random,
+      messageid: "",
+      time:Math.floor(Date.now()/1000)
+    }
+
+    
+    await (<Discord.TextChannel>client.channels.cache.get("722616679280148504")).send(await RandomTemplateEmbed(random, message.channel.id)).then(async message => {
+      await message.react(emojis[7])
+      await message.react('❌')
+      //await message.react('🌀')
+      tempstruct.messageid = message.id
+    })
+
+    await inserttempStruct(tempstruct);
+  
+}
+
+
+
+// export async function RandomTemplateFunc(message: Discord.Message, client: Discord.Client): Promise<string> {
+
+//     let templatelist = await getRandomTemplateList(message, client)
+//     let random = templatelist[Math.floor(Math.random() * (((templatelist.length - 1) - 1) - 1) + 1)]
+
+//     //let guild = client.guilds.cache.get("719406444109103117")
+//     let time = Math.floor(Date.now()/1000)
+
+//     const m = <Discord.Message>(await message.channel.send({ embed: await RandomTemplateEmbed(random) }));
+//     await m.react(emojis[7])
+//     await m.react('🌀')
+
+//     const approve = await m.createReactionCollector(approvefilter, { time: 100000 });
+//     const redo = await m.createReactionCollector(redofilter, { time: 100000 });
+
+
+//     await approve.on("collect", async () => {
+//       await m.reactions.cache.forEach(async (reaction) => {
+//         await reaction.users.remove(message.author.id);
+//         return Promise.resolve(random)
+//         //await message.reply(`reacted`)
+//       });
+//       //m.edit({ embed: await ratingslistEmbed(++page, client, ratings) });
+//     });
+
+//     await redo.on("collect", async () => {
+//       await m.reactions.cache.forEach((reaction) =>
+//         reaction.users.remove(message.author.id)
+//       );
+//       await m.edit({ embed: await getRandomTemplateList(message, client) });
+//       random =
+//         templatelist[
+//           Math.floor(Math.random() * (templatelist.length - 1 - 1 - 1) + 1)
+//         ];
+
+//       let embed = new Discord.MessageEmbed() //For discord v11 Change to new Discord.RichEmbed()
+//         .setImage(random);
+
+//       m.edit(embed);
+//     })
+
+// }
