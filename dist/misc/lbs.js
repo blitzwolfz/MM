1d98667595bb3e9f66767541fe9565df8882cda7
Sam <qureshisamin25@gmail.com>
2020-08-07 01:40:44 -0400

    Removed nickname from lb

--- dist/misc/lbs.js
+++ dist/misc/lbs.js
@@ -22,15 +22,15 @@ async function cockratingLB(message, client, args) {
 }
 exports.cockratingLB = cockratingLB;
 async function ratingslistEmbed(page = 1, client, ratings) {
-    let guild = client.guilds.cache.get("719406444109103117");
     page = page < 1 ? 1 : page;
     const fields = [];
     let index = (0 + page - 1) * 10;
-    for (let i = index; i < index + 10; i++)
+    for (let i = index; i < index + 10; i++) {
         fields.push({
-            name: `${i + 1}) ${(await (await guild.members.fetch(ratings[i]._id)).nickname) || await (await client.users.fetch(ratings[i]._id)).username}`,
+            name: `${i + 1}) ${await (await client.users.fetch(ratings[i]._id)).username}`,
             value: `Cock rating is: ${100 === ratings[i].num ? `100% good cock` : `${ratings[i].num}/${100}`}`
         });
+    }
     return {
         title: `Cock Rating Leaderboard. You are on page ${page || 1} of ${Math.floor(ratings.length / 10) + 1}`,
         description: fields.length === 0 ?
--- src/misc/lbs.ts
+++ src/misc/lbs.ts
@@ -28,16 +28,18 @@ export async function cockratingLB(message: Discord.Message, client: Discord.Cli
 async function ratingslistEmbed(page: number = 1, client: Discord.Client, ratings: cockratingInterface[]){
 
     //let signup = await getSignups()
-    let guild = client.guilds.cache.get("719406444109103117")
+    //let guild = client.guilds.cache.get("719406444109103117")
 
     page = page < 1 ? 1 : page;
     const fields = [];
     let index = (0 + page - 1) * 10
-    for (let i = index; i < index + 10; i++)
+    for (let i = index; i < index + 10; i++){
         fields.push({
-            name: `${i+1}) ${(await (await guild!.members.fetch(ratings[i]._id)).nickname) || await (await client.users.fetch(ratings[i]._id)).username}`,
+            name: `${i+1}) ${await (await client.users.fetch(ratings[i]._id)).username}`,
             value: `Cock rating is: ${100 === ratings[i].num ? `100% good cock` : `${ratings[i].num}/${100}`}`
         });
+    }
+
 
     return {
         title: `Cock Rating Leaderboard. You are on page ${page! || 1} of ${Math.floor(ratings.length / 10) + 1}`,
