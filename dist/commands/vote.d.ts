4d0b379a4d668fa0e3e8e39bd85d838a4b7cbca8
Sam <qureshisamin25@gmail.com>
2021-02-07 23:06:03 -0500

    Removed old files from dist/commands

--- dist/commands/vote.d.ts
+++
@@ -1,2 +0,0 @@
-import * as discord from "discord.js";
-export declare function polls(messageReaction: discord.MessageReaction, user: discord.User | discord.PartialUser, client: discord.Client): Promise<discord.Message | undefined>;
--- dist/misc/commandinterface.d.ts
+++
@@ -1,6 +0,0 @@
-import * as Discord from "discord.js";
-export interface CommandStruct {
-    help(): string;
-    theCommand(command: string): boolean;
-    runCommand(args: string[], message: Discord.Message, client: Discord.Client): Promise<any[]>;
-}
